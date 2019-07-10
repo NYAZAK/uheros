0dd0d48d7faa5dfd2406a9be85e43173ee98fcb4
cla31 <clairerodriguez79@yahoo.fr>
2019-07-10 14:39:36 +0200

    maj Villes

---
+++ src/main/java/com/minijeu/uheros/LieuxPack/NewYork.java
@@ -0,0 +1,17 @@
+package com.minijeu.uheros.LieuxPack;
+
+public class NewYork implements Villes {
+
+
+    @Override
+    public void population(){
+        System.out.println("New York est une ville de 8,623 millions d'habitants");
+    }
+    @Override
+    public void superficie(){
+        System.out.println("New york est une ville d'une superficie de 783,8 km2");
+    }
+
+
+
+}
---
+++ src/main/java/com/minijeu/uheros/LieuxPack/Toulouse.java
@@ -0,0 +1,17 @@
+package com.minijeu.uheros.LieuxPack;
+
+public class Toulouse implements Villes {
+
+
+    @Override
+    public void population(){
+        System.out.println("Toulouse est une ville de 471 941 habitants");
+    }
+    @Override
+    public void superficie(){
+        System.out.println("Toulouse est une ville d'une superficie de 118,3 km2");
+    }
+
+
+
+}
